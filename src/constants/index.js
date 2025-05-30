4c8a92605ff6b4304fd67460508cf8ce8d3db86f
heruarbianto <heru_arbianto@teknokrat.ac.id>
2025-05-30 14:47:43 +0700

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,202 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    twitter,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+
+
+export const socialLinks = [
+    {
+        name: 'twitter',
+        iconUrl: twitter,
+        link: '#',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/heruarbianto',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: '#',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Games RAKA',
+        description: 'RAKA (Raih Kenyang) is a 2-side scrolling adventure game that educates players on the importance of Zero Hunger, waste sorting, and financial management. Set in a vibrant urban environment, players guide Raka, a young hero on a mission to collect food, sort waste correctly, and manage limited resources to survive and thrive. Through engaging gameplay and meaningful challenges, RAKA aims to raise awareness and inspire action toward achieving the Sustainable Development Goals.',
+        link: 'https://games.mmcproject.web.id/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Dictionary  Indonesian ⇌ Javanese',
+        description: 'Indonesian ⇌ Javanese Dictionary is a bilingual digital dictionary designed to facilitate translation between Indonesian and Javanese languages. The project aims to preserve and promote the rich linguistic heritage of the Javanese language by making it more accessible to a wider audience, especially younger generations. Featuring an intuitive interface, the dictionary offers word translations, pronunciation guides, usage examples, and support for different Javanese speech levels (ngoko, krama, krama inggil). It serves as a practical tool for language learners, educators, researchers, and anyone interested in understanding or communicating in Javanese.',
+        link: 'https://dictionary.mmcproject.web.id/',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'HooBank',
+        description: 'Designed and built a Modern UI/UX website using React.js & Tailwind CSS inspired by Figma.',
+        link: 'https://modern-ui-ux-web.vercel.app/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Tuku.YO Cross Platform',
+        description: `tuku.YO is a cross-platform digital ordering system tailored for restaurants, built with a powerful backend API, an easy-to-use Content Management System (CMS), and a responsive Flutter-based mobile and web interface. The platform allows restaurant owners to manage menus, orders, table reservations, and customer interactions in real time. With features like digital menu display, multi-platform ordering (web, Android, iOS), and integrated payment solutions, tuku.YO helps modernize restaurant operations and enhance customer experience. It's designed to be scalable, user-friendly, and adaptable to various types of food businesses—from small cafes to full-service restaurants.`,
+        link: 'https://github.com/heruarbianto/PBS-IF22DX-MMC',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Tuku.YO Web Only',
+        description: 'tuku.YO is a web-based restaurant ordering platform optimized with server-side rendering (SSR) for faster load times and better SEO. Built without a separate API layer, the system delivers a streamlined architecture where frontend and backend logic are tightly integrated, reducing complexity and improving performance. Restaurant owners can manage menus, track orders, and update content directly through a built-in admin panel. Designed for simplicity and speed, tuku.YO is ideal for small to medium-sized restaurants seeking a cost-effective and responsive online presence without the overhead of mobile apps or complex APIs.',
+        link: 'https://github.com/heruarbianto/PW2-IF22DX-MMC',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'MotivAI',
+        description: 'MotivAI is a Flutter-based mobile chat application powered by the Gemini API, designed to provide users with supportive conversations focused exclusively on motivation, personal problem-solving, and general well-being. The AI is intentionally limited to these core topics to ensure safe, empathetic, and purpose-driven interactions. Whether users are seeking encouragement, trying to navigate emotional challenges, or looking for healthy lifestyle tips, MotivAI offers a friendly and non-judgmental space to reflect and grow. With a clean interface and focused AI guidance, the app aims to promote mental clarity and daily positivity through meaningful digital conversations.',
+        link: 'https://github.com/cecep1407/MMC-PM2',
+    }
+];
+export const service = [
+    {
+
+        theme: 'btn-back-red',
+        name: 'Web Development',
+
+    },
+    {
+
+        theme: 'btn-back-red',
+        name: 'UI/UX Design',
+
+    },
+    {
+
+        theme: 'btn-back-red',
+        name: 'Technical Consulting',
+
+    },
+    {
+
+        theme: 'btn-back-red',
+        name: 'Content Creation',
+
+    },
+]
